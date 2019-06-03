112e7648c01c504a2b865768d7db3155518f5120
Flavio H C Mauro <flaviomauro@users.noreply.github.com>
2019-06-02 22:47:34 -0300

    Update arduino-nfc-pn532.ino

--- arduino-nfc-pn532.ino
+++ arduino-nfc-pn532.ino
@@ -1,29 +1,53 @@
-//https://github.com/elechouse/PN532
+/*
+*******************************************************************************
+Description:
+Sample code to show how to read NFC tags with Arduino and PN532.
+GitHub: https://github.com/flaviomauro/arduino-nfc-pn532
+*******************************************************************************
+Item list:
+  1 x Adafruit Feather
+  1 x Micro USB cable
+  1 x Protoboard
+  1 x NFC PN532
+  4 x Jumper cables M/F
 
+External Library list:
+  PN532.h - https://github.com/elechouse/PN532
+  PN532_I2C.h - https://github.com/elechouse/PN532
+
+Additional Actions:
+  Please setup your PN532 switches to I2C.
+
+*******************************************************************************
+@author: Flavio H. C. Mauro
+@date: 10-Jan-2017
+LinkedIn: https://br.linkedin.com/in/flaviomauro
+*******************************************************************************
+*/
 #include <Wire.h>
 #include <PN532_I2C.h>
-#include <PN532.h>   // The following files are included in the libraries Installed
+#include <PN532.h>
 #include <NfcAdapter.h>
 
 PN532_I2C pn532_i2c(Wire);
-NfcAdapter nfc = NfcAdapter(pn532_i2c);  // Indicates the Shield you are using
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
 
 void setup(void) {
   Serial.begin(9600);
-  Serial.println("NFC TAG READER"); // Header used when using the serial monitor
+  Serial.println("NFC TAG READER");
   nfc.begin();
 }
 
 void loop(void) {
-  Serial.println("\nScan your NFC tag on the NFC Shield\n");  // Command so that you an others will know what to do 
+  Serial.println("\nScan your NFC tag\n");
 
   if (nfc.tagPresent())
   {
     NfcTag tag = nfc.read();
     Serial.println(tag.getTagType());
-    Serial.print("UID: ");Serial.println(tag.getUidString()); // Retrieves the Unique Identification from your tag
+    Serial.print("UID: ");Serial.println(tag.getUidString());
 
-    if (tag.hasNdefMessage()) // If your tag has a message
+    if (tag.hasNdefMessage())
     {
 
       NdefMessage message = tag.getNdefMessage();
@@ -35,7 +59,7 @@ void loop(void) {
       }
       Serial.println(".");
 
-      // If you have more than 1 Message then it wil cycle through them
+  
       int recordCount = message.getRecordCount();
       for (int i = 0; i < recordCount; i++)
       {
@@ -47,7 +71,7 @@ void loop(void) {
         record.getPayload(payload);
 
 
-        String payloadAsString = ""; // Processes the message as a string vs as a HEX value
+        String payloadAsString = "";
         for (int c = 0; c < payloadLength; c++) {
           payloadAsString += (char)payload[c];
         }
@@ -57,10 +81,10 @@ void loop(void) {
 
         String uid = record.getId();
         if (uid != "") {
-          Serial.print("  ID: ");Serial.println(uid); // Prints the Unique Identification of the NFC Tag
+          Serial.print("  ID: ");Serial.println(uid);
         }
       }
     }
   }
-  delay(10000);
+  delay(500);
 }
